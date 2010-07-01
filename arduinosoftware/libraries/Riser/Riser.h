5ff1ea593d056da80c27c1b22786f5a9b5c8ee73
jim.gallt <jim.gallt@11eaef69-15cb-b3ff-657e-d52ce9b43f8f>
2010-07-01 22:56:23 +0000

    Added Riser library

---
+++ arduinosoftware/libraries/Riser/Riser.h
@@ -0,0 +1,57 @@
+// Rate of rise calculation
+// Version:  20100701
+
+// *** BSD License ***
+// ------------------------------------------------------------------------------------------
+// Copyright (c) 2010, MLG Properties, LLC
+// All rights reserved.
+//
+// Contributor:  Jim Gallt
+//
+// Redistribution and use in source and binary forms, with or without modification, are 
+// permitted provided that the following conditions are met:
+//
+//   Redistributions of source code must retain the above copyright notice, this list of 
+//   conditions and the following disclaimer.
+//
+//   Redistributions in binary form must reproduce the above copyright notice, this list 
+//   of conditions and the following disclaimer in the documentation and/or other materials 
+//   provided with the distribution.
+//
+//   Neither the name of the MLG Properties, LLC nor the names of its contributors may be 
+//   used to endorse or promote products derived from this software without specific prior 
+//   written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL 
+// THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS 
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// ------------------------------------------------------------------------------------------
+
+#ifndef RISER_H
+#define RISER_H
+
+#define MAX_N 16
+#define SEC_PER_MIN 60
+
+// -------------------------------------------- class Riser
+class Riser {
+  public:
+    Riser( int nsamples );
+    float CalcRate( float tod, float temp );  // calculates current rate of rise
+  private:
+    float integral();  // returns integration of N temp values
+    int N;  // number of samples
+    float samp[2][MAX_N];  // samp[*][0] is oldest sample
+    int n;  // number of samples in the array
+    int nm1;  // N - 1
+    float old_tod;   // midpoint timestamp from previous sample period
+    float old_avgtemp; // avg temp from previous sample period
+};
+
+#endif
