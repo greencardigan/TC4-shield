112830a1be16ed1f95118cfc267e36cfc8e7c89f
jim.gallt <jim.gallt@11eaef69-15cb-b3ff-657e-d52ce9b43f8f>
2010-08-21 02:10:55 +0000

    Significant changes made to cADC library.  Converted everything to one-shot mode on the MCP9800 and MCP3424 for better control and less battery drain.

--- arduinosoftware/libraries/cADC/cADC.cpp
+++ arduinosoftware/libraries/cADC/cADC.cpp
@@ -1,5 +1,5 @@
 // cADC library
-// Version date: July 24, 2010
+// Version date: August 20, 2010
 // interface with MCP3424 18-bit ADC
 
 // *** BSD License ***
@@ -39,189 +39,6 @@
 
 #include <cADC.h>
 
-// ----------------------------------------------------------
-cADC::cADC( uint8_t addr ) {
-  cfg = CFG8;  // select gain = 8 as default
-  a_adc = addr; // address of ADC chip
-  cal_gain = CAL_GAIN;
-  cal_offset = CAL_OFFSET;
-};
-
-// --------------------------------------------------setCal
-void cADC::setCal( float gain, int8_t offs ) {
-  cal_gain = gain;
-  cal_offset = offs;
-};
-
-// -----------------------------------------------------------
-int32_t cADC::getuV( byte& chan ) {
-  int stat;
-  byte a, b, c, gain;
-//  byte rdy, mode, ss;
-  int32_t v;
-  
-  Wire.requestFrom( a_adc, (uint8_t)4 );
-  a = Wire.receive();
-  b = Wire.receive();
-  c = Wire.receive();
-  stat = Wire.receive();
-
-  // rdy = ( stat >> 7 ) & B01;
-  chan = ( stat >> 5 ) & B11;
-  // mode = ( stat >> 4 ) & B01;
-  // ss = ( stat >> 2 ) & B11;
-  gain = stat & B11;
-
-  v = a;
-  v <<= 24;
-  v >>= 16;
-  v |= b;
-  v <<= 8;
-  v |= c;
-
-// convert to microvolts
-  v = round( v * BITS_TO_uV );
-  // divide by gain
-  v >>= gain;
-  // v /= 1 << ( gain );
-  v *= cal_gain;    // calibration of gain
-  v += cal_offset;  // adjust calibration offset
-
-  return v;
-};
-
-// -------------------------------------
-void cADC::nextConversion( byte chan ) {
-  Wire.beginTransmission( a_adc );
-  Wire.send( cfg | ( chan << 5 ) );
-  Wire.endTransmission();
-};
-
-// ------------------------------------------
-void cADC::initADC() {
-  Wire.beginTransmission( a_adc );
-  Wire.send( cfg );
-  Wire.endTransmission();
-};
-
-
-// ----------------------------------------------------------- ambSensor
-ambSensor::ambSensor( uint8_t addr, int navg ) {
- a_amb = addr; // I2C address
- if( nsamp > MAX_AMB ) nsamp = MAX_AMB;
- nsamp = navg; // number of samples for performing averaging
- if( nsamp < 1 ) nsamp = 1;
- temp_offset = TEMP_OFFSET; // fixme read calibration data from EEPROM
- sumamb = 0;
- avgamb = 0;
-};
-
-// ----------------------------------------
-void ambSensor::config() {
-  
-  Wire.beginTransmission(A_AMB);
-  Wire.send(1); // point to config reg
-  Wire.send(A_BITS); 
-  Wire.endTransmission();
-
-/* debugging code disabled
-  // see if we can read it back.
-  byte a;
-  Wire.beginTransmission(A_AMB);
-  Wire.send(1); // point to config reg
-  Wire.endTransmission();
-  Wire.requestFrom(A_AMB, 1);
-  a = 0xff;
-  if (Wire.available()) {
-    a = Wire.receive();
-  }
-  if (a != A_BITS) {
-    Serial.println("# Error configuring mcp9800");
-  } else {
-    Serial.println("# mcp9800 Config reg OK");
-  }
-*/
- 
-};
-
-// ------------------------------------------------
-void ambSensor::init() { // populate array for averaging
- int i;
- int32_t tempC; 
- for( i = 0; i < nsamp; i++ ) {
-  tempC = readAmbientC();
-  delay( MCP9800_DELAY );
-  sumamb += tempC;
-  ambs[i] = tempC;
- }; 
-};
-
-// ----------------------------------------
-int32_t ambSensor::calcAvg() {
- int i;
- if( nsamp <= 1 ) 
-  return avgamb = current;
- else {
-  sumamb += current - ambs[0];
-  for( i = 0; i < nsamp - 1; i++ )
-   ambs[i] = ambs[i+1];
-  ambs[nsamp-1] = current;
-  return avgamb = sumamb / nsamp;
- };
-};
-
-// -----------------------------------------------
-int32_t ambSensor::getCurrent() {
- return current;
-};
-
-// -----------------------------------------
-int32_t ambSensor::readAmbientC() { // returns 16X actual temp
-  byte a, b;
-  int32_t v, va, vb;
-
-  Wire.beginTransmission( a_amb );
-  Wire.send(0); // point to temperature reg.
-  Wire.endTransmission();
-  Wire.requestFrom( a_amb, (uint8_t)2 );
-  va = a = Wire.receive();
-  vb = b = Wire.receive();
-  
-#ifdef RES_12
-// 12-bit code
-  v = ( ( va << 8 ) +  vb ) >> 4; // LSB = 0.0625C
-#endif
-#ifdef RES_11
-// 11-bit code
-  v = ( ( va << 8 ) +  vb ) >> 5; // LSB = 0.125C
-  v <<= 1;
-#endif
-#ifdef RES_10
-// 10-bit code
-  v = ( ( va << 8 ) +  vb ) >> 6; // LSB = 0.25C
-  v <<= 2;
-#endif
-#ifdef RES_9
-// 9-bit code
-  v = ( ( va << 8 ) +  vb ) >> 7; // LSB = 0.5C
-  v <<= 3;
-#endif
-
-// Serial.println( v );
- return current = v;  
-};
-
-// ------------------------------------
-float ambSensor::getOffset() {
- return temp_offset;
-};
-
-// ---------------------------------------
-void ambSensor::setOffset( float tempC ) {
- temp_offset = tempC;
-};
-
-
 // --------------------------------------------------- dFilterRC
 filterRC::filterRC() {
  level = 0;
@@ -239,7 +56,7 @@ void filterRC::init( int32_t percent ) {
 int32_t filterRC::doFilter ( int32_t xi ) {
    if( first) {
       y = xi;
-	first = false;
+  first = false;
       return y;
    }
    float yy = (float)(100 - level) * (float)xi * 0.01;
@@ -247,3 +64,143 @@ int32_t filterRC::doFilter ( int32_t xi ) {
    yy += yyy;
    return y = round( yy );
 };
+
+// ----------------------------------------------------------
+cADC::cADC( uint8_t addr ) {
+  cfg = CFG8;  // select gain = 8 as default
+//  cfg = B10001111;
+  a_adc = addr; // address of ADC chip
+  cal_gain = CAL_GAIN;
+  cal_offset = CAL_OFFSET;
+  initFilter(0); // by default, no filtering
+}
+
+// --------------------------------------------------setCal
+void cADC::setCal( float gain, int8_t offs ) {
+  cal_gain = gain;
+  cal_offset = offs;
+};
+
+// -----------------------------------------------------------
+int32_t cADC::readuV() {
+  uint8_t stat;
+  uint8_t a, b, c, gain;
+  int32_t v;
+  float xv;
+  
+  Wire.requestFrom( a_adc, (uint8_t)4 );
+  a = Wire.receive();
+  b = Wire.receive();
+  c = Wire.receive();
+  stat = Wire.receive();
+  gain = stat & B11;
+
+  v = a;
+  v <<= 24;
+  v >>= 16;
+  v |= b;
+  v <<= 8;
+  v |= c;
+
+// convert to microvolts
+  xv = v;
+  v = round( xv * BITS_TO_uV );
+  // divide by gain
+  v >>= gain;
+  v *= cal_gain;    // calibration of gain
+  v += cal_offset;  // adjust calibration offset
+  raw = v;
+
+  return filtered = filter.doFilter( v );
+};
+
+// --------------------------------------
+int32_t cADC::getRaw() { return raw; }
+int32_t cADC::getFiltered() { return filtered; }
+
+// -------------------------------------
+void cADC::nextConversion( uint8_t chan ) {
+  Wire.beginTransmission( a_adc );
+  Wire.send( cfg | ( chan << 5 ) );
+  Wire.endTransmission();
+};
+
+// ------------------------------------------
+void cADC::initFilter( int fpercent ) {
+  filter.init( fpercent );
+};
+
+// ----------------------------------------------------------- ambSensor
+ambSensor::ambSensor( uint8_t addr ) {
+ a_amb = addr; // I2C address
+};
+
+// ------------------------------------------------
+void ambSensor::init( int fpercent ) {
+  filter.init( fpercent );
+  Wire.beginTransmission( a_amb );
+  Wire.send(1); // point to config reg
+  Wire.send( (uint8_t)SHUTDOWN );  // have to start in shutdown mode for one-shot conversions
+  Wire.endTransmission();
+};
+
+// -----------------------------------------
+void ambSensor::nextConversion() {
+  Wire.beginTransmission( a_amb );
+  Wire.send( 1 ); // configuration register
+  Wire.send( A_BITS ); // request a one-shot conversion
+  Wire.endTransmission();
+}
+
+// -----------------------------------------
+int32_t ambSensor::readSensor() {
+  byte a, b;
+  int32_t va, vb;
+
+  Wire.beginTransmission( a_amb );
+  Wire.send(0); // point to temperature reg.
+  Wire.endTransmission();
+  Wire.requestFrom( a_amb, (uint8_t)2 );
+  va = a = Wire.receive();
+  vb = b = Wire.receive();
+  
+#ifdef RES_12
+// 12-bit code
+  raw = ( ( va << 8 ) +  vb ) >> 4; // LSB = 0.0625C
+#endif
+#ifdef RES_11
+// 11-bit code
+  raw = ( ( va << 8 ) +  vb ) >> 5; // LSB = 0.125C
+  raw <<= 1;
+#endif
+#ifdef RES_10
+// 10-bit code
+  raw = ( ( va << 8 ) +  vb ) >> 6; // LSB = 0.25C
+  raw <<= 2;
+#endif
+#ifdef RES_9
+// 9-bit code
+  raw = ( ( va << 8 ) +  vb ) >> 7; // LSB = 0.5C
+  raw <<= 3;
+#endif
+
+  filtered = filter.doFilter( raw );
+  ambC = (float)filtered * AMB_LSB + temp_offset;
+  ambF = 1.8 * ambC + 32.0;
+  return filtered;
+};
+
+// -----------------------------------
+float ambSensor::getAmbC() { return ambC; }
+float ambSensor::getAmbF() { return ambF; }
+
+// ------------------------------------
+float ambSensor::getOffset() {
+ return temp_offset;
+};
+
+// ---------------------------------------
+void ambSensor::setOffset( float tempC ) {
+ temp_offset = tempC;
+};
+
--- arduinosoftware/libraries/cADC/cADC.h
+++ arduinosoftware/libraries/cADC/cADC.h
@@ -1,5 +1,5 @@
 // cADC library
-// Version date: July 24, 2010
+// Version date: Aug 20, 2010
 // interface with MCP3424 18-bit ADC
 
 // *** BSD License ***
@@ -43,32 +43,63 @@
 #include <Wire.h>
 #include <WProgram.h>
 
+// -------------------------------------------------------------------
+// class for digital filtering of raw signals
+class filterRC {
+public:
+ filterRC();
+ void init( int32_t percent );
+ int32_t doFilter( int32_t xi );
+protected:
+ int32_t level; // filtering level, 0 to 100%
+ int32_t y; // most recent value of function
+ bool first; // special handling of first call
+};
+
 // -------------- ADC configuration
 #define ADC_RDY 7  // ready bit
 #define ADC_C1  6  // channel selection bit 1
 #define ADC_C0  5  // channel selection bit 0
-#define ADC_CMB 4  // conversion mode bit (1 = continuous)
+#define ADC_CMB 4  // conversion mode bit (1 = continuous 0 = one shot)
 #define ADC_SR1 3  // sample rate selection bit 1 (11 = 18 bit)
 #define ADC_SR0 2  // sample rate selection bit 0
 #define ADC_G1  1  // gain select bit 1
 #define ADC_G0  0  // gain select bit 0
 
-#define CFG1 (_BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) ) // 18 bit, gain = 1
-#define CFG2 (_BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G0) ) // 18 bit, gain = 2 
-#define CFG4 (_BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G1) ) // 18 bit, gain = 4 
-#define CFG8 (_BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G1) | _BV(ADC_G0) ) // 18 bit, gain = 8
+/*
+// continuous mode
+#define CFG1 (_BV(ADC_RDY) | _BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) ) // 18 bit, gain = 1
+#define CFG2 (_BV(ADC_RDY) | _BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G0) ) // 18 bit, gain = 2
+#define CFG4 (_BV(ADC_RDY) | _BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G1) ) // 18 bit, gain = 4
+#define CFG8 (_BV(ADC_RDY) | _BV(ADC_CMB) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G1) | _BV(ADC_G0) ) // 18 bit, gain = 8
+*/
 
+// one shot mode
+#define CFG1 (_BV(ADC_RDY) | _BV(ADC_SR1) | _BV(ADC_SR0) ) // 18 bit, gain = 1
+#define CFG2 (_BV(ADC_RDY) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G0) ) // 18 bit, gain = 2
+#define CFG4 (_BV(ADC_RDY) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G1) ) // 18 bit, gain = 4
+#define CFG8 (_BV(ADC_RDY) | _BV(ADC_SR1) | _BV(ADC_SR0) | _BV(ADC_G1) | _BV(ADC_G0) ) // 18 bit, gain = 8
+
+/*
+// continuous mode
 #define A_BITS9  B00000000
 #define A_BITS10 B00100000
 #define A_BITS11 B01000000
 #define A_BITS12 B01100000
+*/
+
+// one-shot mode
+#define A_BITS9  B10000001
+#define A_BITS10 B10100001
+#define A_BITS11 B11000001
+#define A_BITS12 B11100001
+#define SHUTDOWN B00000001 // puts device in shutdown mode
 
 #define BITS_TO_uV 15.625  // LSB = 15.625 uV
 
 #define MAX_CHAN 4 // maximum number of channels to sample
 
 // ---------------------------- calibration of ADC and ambient temp sensor
-// fixme -- put this information in EEPROM
 #define CAL_OFFSET  ( 0 )  // microvolts
 #define CAL_GAIN 1.00
 
@@ -79,18 +110,21 @@
 class cADC {
  public:
   cADC( uint8_t addr = A_ADC ); // constructor
-  void initADC();  // set up the ADC
-  int32_t getuV( byte& chan ); // retrieves sample and converts to uV
-  void nextConversion( byte chan );  // sets up the next conversion
+  void initFilter( int fpercent = 0 );  // set up filtering (optional)
+  int32_t readuV(); // retrieves sample and converts to uV
+  void nextConversion( uint8_t chan );  // requests the next conversion
   void setCal( float gain, int8_t offs ); // sets calibration gain/offset for 50000 uV and 0 uV
+  int32_t getRaw(); // returns unfiltered uV reading
+  int32_t getFiltered(); // returns last filtered uV reading
  protected:
- private:
-  byte cfg;
+  uint8_t cfg;
   uint8_t a_adc;
   float cal_gain;
   int8_t cal_offset;
+  filterRC filter;
+  int32_t raw;
+  int32_t filtered;
 };
-
 
 // -------------------- MCP9800 configuration
 #define RES_12 // use 12-bit resolution by default
@@ -108,52 +142,33 @@ class cADC {
 #define A_BITS A_BITS9
 #endif
 
-// I2C address for MCP9800
-#define A_AMB 0x48
-
-#define MCP9800_DELAY 250 // sample period for MCP9800
-
-#define NAMBIENT 4  // number of ambient samples to be averaged
+#define A_AMB 0x48 // I2C address for MCP9800
+#define MCP9800_DELAY 250 // minimum sample period for MCP9800
 #define TEMP_OFFSET ( 0.0 )  // Celsius offset -- fixme read from EEPROM
-#define MAX_AMB 8 // array size for ambient samples
 #define AMB_LSB 0.0625 // value of MCP9800 LSB in 12-bit mode
 
 // ----------------------------------------------------------
-// this class communicates with MCP9800 ambient sensor and performs averaging
+// this class communicates with MCP9800 ambient sensor and optionally performs filtering
 class ambSensor {
  public:
-  ambSensor( uint8_t addr = A_AMB, int navg = NAMBIENT ); // default constructor
-  void config(); // configure the MCP9800
-  void init(); // initialize array for averaging
-  int32_t calcAvg(); // updates and returns average amb temp
-  int32_t getCurrent(); // returns current value of averaged amb. temp
-  int32_t readAmbientC(); // returns 16x the ambient Celsius temperature
+  ambSensor( uint8_t addr = A_AMB ); // default constructor
+  void init( int fpercent = 0 ); // setup the MCP9800 for one shot conversions
+  void nextConversion(); // instruct 9800 to perform a conversion
+  int32_t readSensor(); // reads chip, returns 16x the ambient Celsius temperature
+  float getAmbF(); // computes and returns current F
+  float getAmbC(); // computes and returns current C
   float getOffset(); // returns calibration information
   void setOffset( float tempC ); // allows override of default offset
  protected:
- private:
-  int nsamp; // number of samples for averaging
-  float temp_offset;  // calibration data (Celsius)
   uint8_t a_amb;  // I2C address
-  int32_t sumamb; // used for averaging
-  int32_t avgamb;
-  int32_t current; // current value of ambient temp
-  int32_t ambs[MAX_AMB];
+  filterRC filter;
+  float temp_offset;  // calibration offset (Celsius)
+  float ambF, ambC; // most recent filtered readings
+  uint32_t filtered; // up to date filtered raw reading
+  uint32_t raw; // most recent raw sensor reading
 };
 
 
-// -------------------------------------------------------------------
-// class for digital filtering of ADC signals
-class filterRC {
-public: 
- filterRC();
- void init( int32_t percent );
- int32_t doFilter( int32_t xi );
-protected:
- int32_t level; // filtering level, 0 to 100%
- int32_t y; // most recent value of function
- bool first; // special handling of first call
-};
 
 
 
