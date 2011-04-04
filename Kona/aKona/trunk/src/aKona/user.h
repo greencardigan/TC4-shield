37610d1124c71310eda409cd6dd8c331c423676d
jim.gallt <jim.gallt@11eaef69-15cb-b3ff-657e-d52ce9b43f8f>
2011-04-04 21:26:14 +0000

    Added Randy's aKona source files.

---
+++ Kona/aKona/trunk/src/aKona/user.h
@@ -0,0 +1,30 @@
+//Randy's User.h file
+
+#ifndef USER_H
+#define USER_H
+#include "WProgram.h" //I'm not sure why, but I couldn't use boolean variables in this file unless I have this
+
+#define MAX_PROFILE 30 // max number of profiles
+
+#define RS 2
+#define ENABLE 4
+#define DB4 7
+#define DB5 8
+#define DB6 12
+#define DB7 13
+
+//compiler options, depending on roaster configuration
+#define LCD4X20  //remove comment on this line if using 4x20 LCD
+//#define ALPENROST
+//#define EEROM
+//#define PORTEXPANDER
+
+#ifdef LCD4X20
+#define ROWS 4  //use this for a 4 row display
+#define COLS 20  //use this for a 20 column display
+#else
+#define ROWS 2  //use this for a 2 row display
+#define COLS 16  // use this for a 16 column display
+#endif  //if 4X20
+
+#endif
