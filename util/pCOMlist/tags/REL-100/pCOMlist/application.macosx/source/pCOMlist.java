2cb3b49849b4dc99ce07c52e377a3f4f073cb42e
jim.gallt <jim.gallt@11eaef69-15cb-b3ff-657e-d52ce9b43f8f>
2011-04-16 23:49:05 +0000

    Copied files into pCOMlist tags/REL-100 in prep for release.

---
+++ util/pCOMlist/tags/REL-100/pCOMlist/application.macosx/source/pCOMlist.java
@@ -0,0 +1,59 @@
+import processing.core.*; 
+import processing.xml.*; 
+
+import processing.serial.*; 
+
+import java.applet.*; 
+import java.awt.Dimension; 
+import java.awt.Frame; 
+import java.awt.event.MouseEvent; 
+import java.awt.event.KeyEvent; 
+import java.awt.event.FocusEvent; 
+import java.awt.Image; 
+import java.io.*; 
+import java.net.*; 
+import java.text.*; 
+import java.util.*; 
+import java.util.zip.*; 
+import java.util.regex.*; 
+
+public class pCOMlist extends PApplet {
+
+// program lists the COM ports on the machine
+
+// Author:  Jim Gallt 20100705
+// adapted from code written by Bill Welch
+
+
+Serial comport;
+String[] portlist;
+PFont listfont;
+String portstrings;
+
+
+public void setup() {
+  portstrings = "Available COM ports:\n";
+  portlist = Serial.list();
+  println( portlist );
+  for( int i = 0; i < portlist.length; i++ ) {
+    portstrings += portlist[i] + "\n";
+  };
+  size( 200,400 );
+  frameRate( 1 );
+  smooth();
+  background( 100 );
+  fill( 255 );
+  listfont = createFont("Tahoma-Bold",16);
+  textLeading( 5 );
+  textFont( listfont );
+}
+
+public void draw() {
+  text( portstrings, 5, 20, 400, 400 );
+}
+
+
+  static public void main(String args[]) {
+    PApplet.main(new String[] { "--bgcolor=#ECE9D8", "pCOMlist" });
+  }
+}
